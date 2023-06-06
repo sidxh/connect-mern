2ee68e9555a8a39f7e06e787b86de3fa391c8bc2
sidxh <totallysiddhant@gmail.com>
2023-06-06 17:00:56 +0530

    added tabs in the card components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,72 @@
-import React from "react"
-import Tilt from "react-parallax-tilt";
+// import React from "react"
+// import Tilt from "react-parallax-tilt";
 
-export default function Cards(props){
-    return(
-        <div className="cards_component">
-            <img src={require("../images/user2.png")} />
-            <h1>{props.name}</h1>
-            <ul>
-                <li>Line1</li>
-                <li>Line2</li>
-                <li>Line3</li>
+// export default function Cards(props){
+//     return(
+//         <div className="cards_component">
+//             <img src={require("../images/user2.png")} />
+//             <h1>{props.name}</h1>
+//             <ul>
+//                 <li>Line1</li>
+//                 <li>Line2</li>
+//                 <li>Line3</li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+
+import React, { useState } from 'react';
+
+const Cards = (props) => {
+  const [activeTab, setActiveTab] = useState('experience');
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className="cards_component">
+      <img src={require("../images/user2.png")} alt="User" />
+      <h1>{props.name}</h1>
+      <div className="tabs">
+        <button
+          className={`tab ${activeTab === 'experience' ? 'active' : ''}`}
+          onClick={() => handleTabChange('experience')}
+        >
+          Experience
+        </button>
+        <button
+          className={`tab ${activeTab === 'contact' ? 'active' : ''}`}
+          onClick={() => handleTabChange('contact')}
+        >
+          Contact
+        </button>
+      </div>
+      <div className="tab-content">
+        {activeTab === 'experience' && (
+          <div>
+            {/* Render experience content here */}
+            <ul className='exp_ul'>
+              <li>Line 1</li>
+              <li>Line 2</li>
+              <li>Line 3</li>
             </ul>
-        </div>
-    )
-}
+          </div>
+        )}
+        {activeTab === 'contact' && (
+          <div className='contact'>
+            <h3>Contact</h3>
+            {/* Render contact content here */}
+            <p>Contact information</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
+export default Cards;
 
 
 {/*         <div className="cards_component">
